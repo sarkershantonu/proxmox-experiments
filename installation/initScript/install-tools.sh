50e4f137c17a81923ea8f5535cc2c6a5a81e078d
Shantonu Sarker <sarker.shantonu@gmail.com>
2026-02-03 10:25:10 -0600

    Create install-tools.sh

---
+++ installation/initScript/install-tools.sh
@@ -0,0 +1,2 @@
+apt update && apt -y upgrade && apt autoremove
+apt -y install lm-sensors iperf3 speedtest-cli smartmontools ntfs-3g curl git-all software-properties-common p7zip-full cifs-utils net-tools build-essential 
